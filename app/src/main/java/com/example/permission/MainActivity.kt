dfe732d7d8f7d0f8ea42cd9cb51dbf33d1d31e28
Woongchicken <twins1th@naver.com>
2023-07-02 17:00:27 +0900

    2th commit

--- app/src/main/java/com/example/permission/MainActivity.kt
+++ app/src/main/java/com/example/permission/MainActivity.kt
@@ -1,95 +1,131 @@
 package com.example.permission
 
 import android.os.Bundle
-import android.util.Log
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.permission.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
-    val REQUEST_CODE = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.permissionButton.setOnClickListener{
-            permissionCheck()
-        }
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
-    /* 권한 승인 여부 */
-    private fun permissionCheck() {
-        if (versionCehck()){
-            if (checkSelfPermission()) {
 
-                Log.d(Constants.TAG, "${this::class.simpleName} " + "권한 : 승인 상태")
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-                ...
 
-            } else {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+//
+//        }
 
-                Log.d(Constants.TAG, "${this::class.simpleName} " + "권한 : 거절 상태")
-
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        PERMISSIONS.toString())) {
-                    // 1. 사용자가 승인 거절을 누른 상태
-
-                    Log.d(Constants.TAG, "${this::class.simpleName} " + "사용자가 승인 거절을 누른 경우")
-
-                    requestPermissions()
-
+            binding.locationIcon.setOnClickListener {
+                if(isAllPermissionsGrated()) {
+                    Snackbar.make(binding.root,"Permission granted", Snackbar.LENGTH_SHORT).show()
                 } else {
-                    // 2. 사용자가 승인 거절 + 다시 표시하지 않기를 누른 경우
-                    // 3. 아직 승인 요청을 한 적이 없는 경우
+//                requestDangerousPermissions()
+                    requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
+                }
+            }
 
-                    Log.d(Constants.TAG, "${this::class.simpleName} " + "사용자가 승인 거절 + 다시 표시하지 않기를 누른 경우")
 
-                    requestPermissions()
+    }
+
+
+    companion object {
+        private val REQUIRED_PERMISSIONS : Array<String> = arrayOf(
+//            Manifest.permission.ACCESS_COARSE_LOCATION,
+//            Manifest.permission.ACCESS_FINE_LOCATION
+//            Manifest.permission.READ_CALENDAR,
+//            Manifest.permission.READ_BASIC_PHONE_STATE
+
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.READ_CONTACTS
+
+        )
+        private const val REQUEST_CODE_PERMISSIONS = 1001
+    }
+
+
+    /* 권한 부여 여부 확인 */
+    private fun isAllPermissionsGrated() : Boolean = REQUIRED_PERMISSIONS.all { permission ->      // .all을 써서 REQUIRED_PERMISSIONS의 권한 중 하나라도 권한이 부여가 안 되어있으면 false 반환
+        ContextCompat.checkSelfPermission(this,permission) ==       // 해당 권한이 부여되었는지 확인
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    /* 권한 요청 */
+    private  fun requestDangerousPermissions() {
+        ActivityCompat.requestPermissions(
+            this,REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+        )
+    }
+
+    /* 권한 요청에 대한 콜백 */
+    @RequiresApi(Build.VERSION_CODES.M)             // onRequestPermissionsResult - androidx.activity 버전 1.2.0 - deprecated
+    override fun onRequestPermissionsResult(        // ActivityCompat.requestPermissions을 요청하면 결과를 콜백으로 받음
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {        // 모든 권한이 취득된 경우
+                Snackbar.make(binding.root,"Permission granted", Snackbar.LENGTH_SHORT).show()
+            } else {
+                if (shouldShowRequestPermissionRationale(REQUIRED_PERMISSIONS[0])) {        // 권한을 다시 요청
+                    Snackbar.make(binding.root,"Permission required to use app!", Snackbar.LENGTH_SHORT).show()
+                    requestDangerousPermissions()
+                } else {
+                    Snackbar.make(binding.root, "Permission denied", Snackbar.LENGTH_SHORT).show()      // 그럼에도 유저가 거절하면, 앱은 권한을 더 이상 요청을 못함. 권한 요청을 2번하면 유저가 직접 셋팅에서 권한 설정
+                    openSetings()       // 권한 설정 화면 띄워줌
                 }
             }
         }
     }
 
+    /* 계약서 전달 방식 */
+    @RequiresApi(Build.VERSION_CODES.M)
+    private val requestPermissionLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->   // ActivityResultContracts - 미리 정의된 계약서를 전달을 통해, Activity에서 결과를 쉽게 가져올수 있음. 계약서를 취득해서 권한을 설정
+            permissions.entries.forEach{permission->                                                       // RequestMultiplePermissions : 여러개,  RequestPermission : 한개
+                when {
+                    permission.value -> {       // 권한 승인
+                        Snackbar.make(binding.root, "Permission granted", Snackbar.LENGTH_SHORT).show()
+                    }
+                    shouldShowRequestPermissionRationale(permission.key) -> {       // 권한 거부
+                        Snackbar.make(binding.root, "Permission required to use app!", Snackbar.LENGTH_SHORT).show()
+                    }       // 2번 거부
+                    else -> {
+                        Snackbar.make(binding.root, "Permission denied", Snackbar.LENGTH_SHORT).show()
+                        openSetings()
+                    }
 
-    /* SDK 버전 확인 */
-    private fun versionCehck() : Boolean {
-        if (android.os.Build.VERSION.SDK_INT >= SDK_VERSION) {
-            return true
-        }else{
-            return false
-        }
-    }
-
-    /* 권한 승인 여부 확인 */
-    private fun checkSelfPermission(): Boolean {
-        Log.d(Constants.TAG, "${this::class.simpleName} 권한 체크를 시작합니다.")
-
-        PERMISSIONS.forEach {
-            if (ContextCompat.checkSelfPermission(
-                    this@MainActivity, it) != PackageManager.PERMISSION_GRANTED) {
-
-                Log.d(Constants.TAG, "${this::class.simpleName} $it 권한 : 거절")
-
-                return false
+                }
             }
         }
-        return true
+
+
+    /* 권한 설정 화면 */
+    private fun openSetings() {
+        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            data = Uri.fromParts("package",packageName,null)
+        }.run (::startActivity)
     }
-
-    /* 사용자에게 권한 요청 */
-    private fun requestPermissions() {
-        Log.d(Constants.TAG, "${this::class.simpleName} 권한 요청을 시작합니다.")
-        ActivityCompat.requestPermissions(
-            this,
-            PERMISSIONS,
-            Constants.REQUEST_CODE)
-    }
-
-
 
 }
